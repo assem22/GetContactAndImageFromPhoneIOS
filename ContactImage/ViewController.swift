fd22c98814a36a79893b234781efcd0f67b94345
assem22 <assem.mukhamadi@mail.ru>
2020-11-24 08:46:20 +0600

    Initial Commit

---
+++ ContactImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactImage
+//
+//  Created by Assem Mukhamadi on 24.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
